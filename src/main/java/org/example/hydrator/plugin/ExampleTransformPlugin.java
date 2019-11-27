99131d915c951dc5e99b017fa1569a3e6ce9278b
Bajram Bojku <bajrambojku@Bajrams-MacBook-Pro.local>
2019-11-27 17:31:02 -0600

    first commit

---
+++ src/main/java/org/example/hydrator/plugin/ExampleTransformPlugin.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.example.hydrator.plugin;
+
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.api.data.format.StructuredRecord;
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.api.plugin.PluginConfig;
+import io.cdap.cdap.etl.api.Emitter;
+import io.cdap.cdap.etl.api.PipelineConfigurer;
+import io.cdap.cdap.etl.api.Transform;
+import io.cdap.cdap.etl.api.TransformContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soulwing.rot13.Rot13;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+import javax.annotation.Nullable;
+
+/**
+ * Hydrator Transform Plugin Example - This provides a good starting point for building your own Transform Plugin
+ * For full documentation, check out: https://docs.cask.co/cdap/current/en/developer-manual/pipelines/developing-plugins/index.html
+ */
+@Plugin(type = Transform.PLUGIN_TYPE)
+@Name("ExampleTransform") // <- NOTE: The name of the plugin should match the name of the docs and widget json files.
+@Description("This is an example transform.")
+public class ExampleTransformPlugin extends Transform<StructuredRecord, StructuredRecord> {
+  // If you want to log things, you will need this line
+  private static final Logger LOG = LoggerFactory.getLogger(ExampleTransformPlugin.class);
+
+  // Usually, you will need a private variable to store the config that was passed to your class
+  private final Config config;
+  private Schema outputSchema;
+
+  // Mapping of input field to encoder type.
+    private final Map<String, EncodeType> encodeMap = new TreeMap<>();
+
+  public ExampleTransformPlugin(Config config) {
+    this.config = config;
+  }
+
+  /**
+   * This function is called when the pipeline is published. You should use this for validating the config and setting
+   * additional parameters in pipelineConfigurer.getStageConfigurer(). Those parameters will be stored and will be made
+   * available to your plugin during runtime via the TransformContext. Any errors thrown here will stop the pipeline
+   * from being published.
+   * @param pipelineConfigurer Configures an ETL Pipeline. Allows adding datasets and streams and storing parameters
+   * @throws IllegalArgumentException If the config is invalid.
+   */
+  @Override
+  public void configurePipeline(PipelineConfigurer pipelineConfigurer) throws IllegalArgumentException {
+    super.configurePipeline(pipelineConfigurer);
+    // It's usually a good idea to validate the configuration at this point. It will stop the pipeline from being
+    // published if this throws an error.
+    Schema inputSchema = pipelineConfigurer.getStageConfigurer().getInputSchema();
+    config.validate(inputSchema);
+    try {
+      pipelineConfigurer.getStageConfigurer().setOutputSchema(Schema.parseJson(config.schema));
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Output schema cannot be parsed.", e);
+    }
+  }
+
+  /**
+   * This function is called when the pipeline has started. The values configured in here will be made available to the
+   * transform function. Use this for initializing costly objects and opening connections that will be reused.
+   * @param context Context for a pipeline stage, providing access to information about the stage, metrics, and plugins.
+   * @throws Exception If there are any issues before starting the pipeline.
+   */
+  @Override
+  public void initialize(TransformContext context) throws Exception {
+    super.initialize(context);
+    outputSchema = Schema.parseJson(config.schema);
+  }
+
+  /**
+   * This is the method that is called for every record in the pipeline and allows you to make any transformations
+   * you need and emit one or more records to the next stage.
+   * @param input The record that is coming into the plugin
+   * @param emitter An emitter allowing you to emit one or more records to the next stage
+   * @throws Exception
+   */
+  @Override
+  public void transform(StructuredRecord input, Emitter<StructuredRecord> emitter) throws Exception {
+    // Get all the fields that are in the output schema
+        List<Schema.Field> fields = outputSchema.getFields();
+        // Create a builder for creating the output record
+        StructuredRecord.Builder builder = StructuredRecord.builder(outputSchema);
+
+
+
+    for(Schema.Field field : input.getSchema().getFields()) {
+            String fieldName = field.getName();
+            String input_value = input.get(fieldName).toString() ;
+            String output_value = Rot13.rotate(input_value);
+
+         if((config.mask.contains(fieldName)))
+            builder.set(fieldName, output_value);
+          
+         else{
+            builder.set(fieldName, input_value);
+
+         }
+     }
+
+      // Finally, build and emit the record.
+      emitter.emit(builder.build());
+    }
+
+  /**
+     * Defines encoding types supported.
+     */
+    private enum EncodeType {
+    ROT13("ROT13"),
+    NONE("NONE");
+
+    private String type;
+
+    EncodeType(String type) {
+      this.type = type;
+    }
+
+    String getType() {
+      return type;
+    }
+
+
+  }
+    /**
+   * This function will be called at the end of the pipeline. You can use it to clean up any variables or connections.
+   */
+  @Override
+  public void destroy() {
+    // No Op
+  }
+
+  /**
+   * Your plugin's configuration class. The fields here will correspond to the fields in the UI for configuring the
+   * plugin.
+   */
+  public static class Config extends PluginConfig {
+    @Name("mask")
+    @Description("This option is required for this transform.")
+    @Macro // <- Macro means that the value will be substituted at runtime by the user.
+    @Nullable
+    private final String mask;
+
+//    @Name("myOptionalOption")
+//    @Description("And this option is not.")
+//    @Macro
+//    @Nullable // <- Indicates that the config param is optional
+//    private final Integer myOptionalOption;
+
+    @Name("schema")
+    @Description("Specifies the schema of the records outputted from this plugin.")
+    private final String schema;
+
+    public Config(String mask, String schema) {
+      this.mask = mask;
+//      this.myOptionalOption = myOptionalOption;
+      this.schema = schema;
+    }
+
+    private void validate(Schema inputSchema) throws IllegalArgumentException {
+      // It's usually a good idea to check the schema. Sometimes users edit
+      // the JSON config directly and make mistakes.
+      try {
+        Schema.parseJson(schema);
+      } catch (IOException e) {
+        throw new IllegalArgumentException("Output schema cannot be parsed.", e);
+      }
+      // This method should be used to validate that the configuration is valid.
+      if (mask == null || mask.isEmpty()) {
+        throw new IllegalArgumentException("mask is a required field.");
+      }
+      // You can use the containsMacro() function to determine if you can validate at deploy time or runtime.
+      // If your plugin depends on fields from the input schema being present or the right type, use inputSchema
+    }
+  }
+}
+
